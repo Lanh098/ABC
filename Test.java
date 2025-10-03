2052e2a2c3e96f9fb7ef6d701aa8565a866b28d8
PhNhatThinh <phamnhatthinh876@gmail.com>
2025-10-03 17:03:54 +0700

    Bai TH_T5

---
+++ Test.java
@@ -0,0 +1,22 @@
+package th_t5;
+
+import java.util.Scanner;
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap ma sach: ");
+        String maSach = sc.nextLine();
+        System.out.print("Nhap tieu de sach: ");
+        String tieuDe = sc.nextLine();
+        System.out.print("Nhap tac gia: ");
+        String tacGia = sc.nextLine();
+        System.out.print("Nhap nam xuat ban: ");
+        int namXuatBan = sc.nextInt();
+        System.out.print("Nhap so luong: ");
+        int soLuong = sc.nextInt();
+        Sach sach = new Sach(maSach, tieuDe, tacGia, namXuatBan, soLuong);
+        System.out.println("\n===== Thong tin sach vua nhap =====");
+        sach.hienThiThongTin();
+        sc.close();
+    }
+}
