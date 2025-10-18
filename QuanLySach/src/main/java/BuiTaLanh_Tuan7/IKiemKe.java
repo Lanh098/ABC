2073bc6cd628783229b9ee90cf7c7061dfeaf9b7
Lanh098 <buitalanh098@gmail.com>
2025-10-18 20:26:34 +0700

    Bài tập tuần 7

---
+++ QuanLySach/src/main/java/BuiTaLanh_Tuan7/IKiemKe.java
@@ -0,0 +1,15 @@
+package BuiTaLanh_Tuan7;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Bùi Tá Lanh
+ */
+public interface IKiemKe {
+    boolean kiemTraTonKho(int soLuongToiThieu); 
+    void capNhatViTri(String viTriMoi);      
+}
